7ad4d38a07b13ecfcb82e3563b0e2a932a56d5a4
Frakist <90983918+Frakist@users.noreply.github.com>
2023-10-04 16:18:39 +0300

    FARD again

--- songs/help me pls.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-for i in os.scandir("./"):
-    for j in os.scandir("./"+i.name+"/charts"):
-        os.rename("./"+i.name+"/charts/"+j.name, "./"+i.name+"/charts/"+j.name[len(i.name):])
--- songs/helpmepls.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-for i in os.scandir("./"):
-    if not ".py" in i.name: 
-        for j in os.scandir("./"+i.name+"/charts"):
-            if not 'hard' in j.name: os.remove(j)
