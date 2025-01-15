80d6f3a4a783dc9d746f13d31e6d95ed4b0c3503
earframe <102866355+HELLISH666-warframe@users.noreply.github.com>
2025-01-15 18:32:48 +1100

    added_stupid_"-"

--- songs/label.hx
+++ songs/label.hx
@@ -3,7 +3,7 @@ import flixel.text.FlxText.FlxTextFormatMarkerPair;
 import flixel.text.FlxTextBorderStyle;
 
 if ((curSong == 'ron-classic') || (curSong == 'wasted-classic') || (curSong == 'ayo-classic') || (curSong == 'bloodshed-classic') || (curSong == 'trojan-virus-classic') || (curSong == 'bleeding-classic'))
-var kadeshit:FlxText = new FlxText(20, 5, 0, PlayState.SONG.meta.displayName +  " " + PlayState.difficulty.toUpperCase() + " " +  "| KE 1.5.4 (ron eidtion)",12);
+var kadeshit:FlxText = new FlxText(20, 5, 0, PlayState.SONG.meta.displayName +  " - " + PlayState.difficulty.toUpperCase() + " " +  "| KE 1.5.4 (ron eidtion)",12);
 for(k=>label in [kadeshit,]) {
 	label.setFormat(Paths.font("vcr.ttf"), 16,  FlxColor.WHITE, "center", FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
 
