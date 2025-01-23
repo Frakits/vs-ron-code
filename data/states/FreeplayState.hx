6197e0b80761cbcb63e082f1f21605e23d80105d
HELLISH666-warframe <102866355+HELLISH666-warframe@users.noreply.github.com>
2025-01-23 16:19:12 +1100

    fixed_some_things_and_made_freeplay_more_accurate

--- data/states/FreeplayState.hx
+++ data/states/FreeplayState.hx
@@ -1,26 +1,53 @@
+//import
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
 import flixel.graphics.FlxGraphic;
 import flixel.effects.particles.FlxParticle;
 import flixel.effects.particles.FlxTypedEmitter;
+import flixel.text.FlxTextAlign;
+
+//shaders
+var time:Float = 0;
+var crt:CustomShader  = new CustomShader("fake CRT");
+var chrom:CustomShader  = new CustomShader("chromatic aberration");
+var fish:CustomShader  = new CustomShader("fisheye");
+var wig:CustomShader  = new CustomShader("glitchsmh");
+var grey:CustomShader  = new CustomShader("grayscale");
+//cam
+var camWhat:FlxCamera;
 var camText:FlxCamera = new FlxCamera();
+camText.bgColor = null;
+camWhat = new FlxCamera();
+FlxG.cameras.reset(camWhat);
+FlxCamera.defaultCameras = [camWhat];
 var portrait:FlxSprite;
 var preload = [];
-var time:Float = 0;
-//i_know_this_looks_ass_but_it_works
-var fish:CustomShader  = new CustomShader("fisheye");
-camText.addShader(fish);
-fish.data.MAX_POWER.value = [0.19];
-if (FlxG.save.data.crt){ var crt:CustomShader  = new CustomShader("fake CRT");
-	FlxG.camera.addShader(crt);}
-if (FlxG.save.data.chrom) { var chrom:CustomShader  = new CustomShader("chromatic aberration");
-	FlxG.camera.addShader(chrom);camText.addShader(chrom);
-	chrom.data.rOffset.value = [1/2];
-	chrom.data.gOffset.value = [0.0];
-	chrom.data.bOffset.value = [1 * -1];
-}
-camText.bgColor = 0;
+var particles:FlxTypedEmitter;
 override function update(elapsed:Float){time += elapsed;
 	chrom.data.rOffset.value = [0.005*Math.sin(time)];
 	chrom.data.bOffset.value = [-0.005*Math.sin(time)];
+	wig.data.iTime.value = [0.005*Math.sin(time)];
+for (i in 0...songs.length)
+	{
+		grpSongs.members[i].y += (Math.sin(i+time)/2);
+	}
+}
+function shadering() 
+{
+	var curSong = songs[curSelected].displayName;
+	switch(curSong)
+	{
+		case "gron":{ if (FlxG.save.data.grey){FlxG.camera.addShader(grey);camText.addShader(grey);}}
+		case "trojan-virus" | "Bleeding":{ if (FlxG.save.data.glitch){FlxG.camera.addShader(wig);
+		wig.data.iTime.value = [2,2];wig.data.on.value = [1.];}}
+		default:{FlxG.camera.removeShader(grey);camText.removeShader(grey);FlxG.camera.removeShader(wig);
+		wig.data.iTime.value = [2,2];wig.data.on.value = [0];}	
+	}
+	diffText.color = switch(diffText.text)
+	{
+		case 'COOL':0xE00020;
+		default: 0xFFFFFFFF;		
+	}
 }
 function postCreate() {
 	FlxG.cameras.add(camText, false);
@@ -36,12 +63,24 @@ function postCreate() {
 	bg.screenCenter();
 	add(bg);
 
-
 	portrait = new FlxSprite().loadGraphic(Paths.image('menus/freeplay/portraits/ron'));
 	portrait.scale.set(0.51,0.51);
 	portrait.updateHitbox();
 	add(portrait);
 	
+	scoreText = new FlxText(FlxG.width * 0.7, 5, 0, "", 32);
+	scoreText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, "RIGHT");
+
+	scoreBG = new FlxSprite(scoreText.x - 6, 0).makeGraphic(1, 1, 0xFF000000);
+	scoreBG.alpha = 0.3;
+	add(scoreBG);
+
+	diffText = new FlxText(scoreText.x, scoreText.y + 36, 0, "", 24);
+	diffText.font = scoreText.font;
+	add(diffText);
+
+	add(scoreText);
+
 	var bar:FlxSprite = CoolUtil.loadAnimatedGraphic(new FlxSprite(), Paths.image('menus/freeplay/bar'));
 	add(bar);
 	bar.screenCenter();
@@ -52,6 +91,16 @@ function postCreate() {
 		preload.push(graphic);
 	}
 	changeSelection(0, true);
+	{
+		if (FlxG.save.data.crt){FlxG.camera.addShader(crt);}
+		if (FlxG.save.data.chrom) {FlxG.camera.addShader(chrom);camText.addShader(chrom);
+			chrom.data.rOffset.value = [1/2];
+			chrom.data.gOffset.value = [0.0];
+			chrom.data.bOffset.value = [1 * -1];
+		}
+			camText.addShader(fish);
+			fish.data.MAX_POWER.value = [0.19];
+		}
 	var coolemitter:FlxTypedEmitter = new FlxTypedEmitter();
 	coolemitter.width = FlxG.width*1.5;
 	coolemitter.velocity.set(0, -5, 0, -10);
@@ -95,5 +144,6 @@ function onChangeSelection(event) {
 		portrait.y -= 20;
 		portrait.angle = -5;
 		FlxTween.tween(portrait, {y: mfwY, angle: 0}, 0.4, {ease: FlxEase.elasticOut});
+		shadering();
 	}});
 }
