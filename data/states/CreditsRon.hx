7159a9f94e30752cf56fee1b03dccc465d339336
HELLISH666-warframe <102866355+HELLISH666-warframe@users.noreply.github.com>
2025-03-30 17:22:13 +1100

    credits_thingie

--- data/states/CreditsRon.hx
+++ data/states/CreditsRon.hx
@@ -1,17 +1,7 @@
-
-import flixel.addons.display.FlxBackdrop;
-import flixel.util.FlxTimer;
-import flixel.util.FlxColor;
-import flixel.FlxG;
-import haxe.Json;
-import openfl.Assets;
-import flixel.FlxSprite;
-import flixel.math.FlxMath;
 import flixel.math.FlxRandom;
-import flixel.text.FlxText;    
 import openfl.display.BitmapData; 
-import flixel.tweens.FlxTween; 
-import flixel.tweens.FlxEase;           
+import funkin.backend.utils.DiscordUtil;
+import Alphabetthing;
 	var creditJSON:Dynamic;
 	var nameGroup = [];
 	var curSelected:Int = 0;
@@ -21,9 +11,11 @@ import flixel.tweens.FlxEase;
 	var socialMediaText:FlxText;
 	var socialMediaFavicon:FlxSprite;
 	var time:Float = 0;
+	var lerpVal:Float = FlxMath.bound(time * 8., 0, 1);
 	var	bg:FlxSprite;
 
 	override function create() {
+		DiscordUtil.changePresence('Looking at Credits', null);
 		bg = new FlxSprite();
 		bg.frames = Paths.getSparrowAtlas('menus/freeplay/classicbgAnimate');
 		bg.color = FlxColor.RED;
@@ -35,18 +27,16 @@ import flixel.tweens.FlxEase;
 		add(bg);
 		creditJSON = Json.parse(Assets.getText(Paths.json("credit")));
 		for (i in 0...creditJSON.length){
-			var j = new Alphabet(0, 100 + (150 * i), creditJSON[i].handle,true);
+			var j = new Alphabetthing(0, 100 + (150 * i), creditJSON[i].handle,true);
 			j.ID = i;
-			j.targetY = -100;
+			j.forceX = 100;
 			nameGroup.push(j);
 			add(j);
 			var e = new FlxSprite().loadGraphic(Paths.image("credits/" + creditJSON[i].name));
 			e.setGraphicSize(50, 50);
 			e.updateHitbox();
 			e.antialiasing = true;
-			e.x = curSelected + 190;
-			j.x = socialMediaText + 490;
-//			j.trackingSpr = e;
+			j.trackingSpr = e;
 			add(e);
 		}
 		largePortrait = new FlxSprite(800, 20).loadGraphic(Paths.image("credits/seezee"));
@@ -67,7 +57,6 @@ import flixel.tweens.FlxEase;
 		add(largePortrait);
 		add(descText);
 		add(dividingBar);
-//		super.create();
 
 		changeSelection(0);
 	}
@@ -87,52 +76,51 @@ import flixel.tweens.FlxEase;
 		for (j in nameGroup) {
 			j.y = FlxMath.lerp(j.y, 360 + (150 * (j.ID - curSelected)), 0.1 / (1));
 			if (j.text != null)
-				if (!antiSpam) j.scale.set(FlxMath.lerp(j.scale.x, (4 - Math.abs(j.ID - curSelected)) * (0.3 - (j.text.length * 0.01)), 0.2 / (1)), FlxMath.lerp(j.scale.y, (4 - Math.abs(j.ID - curSelected)) * (0.3 - (j.text.length * 0.01)), 0.05 / (1/2)));
-			j.targetY = 20+FlxMath.lerp(j.targetY, 100 + -Math.abs(25 * (j.ID - curSelected)), 0.2 / (1));
+				if (!antiSpam) j.scale.set(FlxMath.lerp(j.scale.x, (4 - Math.abs(j.ID - curSelected)) * (0.3 - (j.text.length * 0.01)), 0.2 / (1)), FlxMath.lerp(j.scale.y, (4 - Math.abs(j.ID - curSelected)) * (0.3 - (j.text.length * 0.01)), 0.05 / (1)));
+			j.forceX = 20+FlxMath.lerp(j.forceX, 100 + -Math.abs(25 * (j.ID - curSelected)), 0.2 / (1));
 		}
-//		super.update(elapsed);
 	}
-	function changeSelection(e) {
-		curSelected += e;
-		if (curSelected > nameGroup.length - 1) keyCount += 1;
-			if (curSelected == 7 && keyCount > 4 && time < 3 && !antiSpam) {
-			FlxG.camera.shake(0.05, 3, function() {
-				FlxG.camera.flash();
-				FlxG.sound.play(Paths.sound("boom"));
-				for (i in nameGroup) {i.autoOffset = false;
-					for (j in i) {j.offset.set(0,0); j.scale.set(new FlxRandom().float(0.4,2),new FlxRandom().float(0.4,2));
-						FlxTween.tween(j, {x: j.x + new FlxRandom().int(-1750, 1750), y: j.y + new FlxRandom().int(-1750, 1750), angle: new FlxRandom().int(360, -360)}, 5, {ease: FlxEase.quadOut});}}
-				new FlxTimer().start(5.1, function(tmr:FlxTimer) {
-					FlxG.sound.play(Paths.sound("rumble"));
-					FlxG.sound.play(Paths.sound("piecedTogether"));
-					FlxG.camera.fade(0xFFFFFF, 3.8, false, function() {#if windows Sys.exit(0); #end});
-					for (i in nameGroup) 
-						for (j in i) FlxTween.tween(j, {x: new FlxRandom().float(495, 515), y: new FlxRandom().float(275, 300)}, new FlxRandom().float(1, 3), {ease: FlxEase.quintInOut});
-				});
+function changeSelection(e) {
+	curSelected += e;
+	if (curSelected > nameGroup.length - 1) keyCount += 1;
+		if (curSelected == 7 && keyCount > 4 && time < 3 && !antiSpam) {
+		FlxG.camera.shake(0.05, 3, function() {
+			FlxG.camera.flash();
+			FlxG.sound.play(Paths.sound("boom"));
+			for (i in nameGroup) {i.autoOffset = false;
+				for (j in i) {j.offset.set(0,0); j.scale.set(new FlxRandom().float(0.4,2),new FlxRandom().float(0.4,2));
+					FlxTween.tween(j, {x: j.x + new FlxRandom().int(-1750, 1750), y: j.y + new FlxRandom().int(-1750, 1750), angle: new FlxRandom().int(360, -360)}, 5, {ease: FlxEase.quadOut});}}
+			new FlxTimer().start(5.1, function(tmr:FlxTimer) {
+				FlxG.sound.play(Paths.sound("rumble"));
+				FlxG.sound.play(Paths.sound("piecedTogether"));
+				FlxG.camera.fade(0xFFFFFF, 3.8, false, function() {#if windows Sys.exit(0); #end});
+				for (i in nameGroup) 
+					for (j in i) FlxTween.tween(j, {x: new FlxRandom().float(495, 515), y: new FlxRandom().float(275, 300)}, new FlxRandom().float(1, 3), {ease: FlxEase.quintInOut});
 			});
-			FlxG.sound.play(Paths.sound("rumble"));	
-			antiSpam = true;
-			for (j in nameGroup)
-					j.scale.set(FlxMath.lerp(j.scale.x, (4 - Math.abs(j.ID - curSelected)) * (0.3 - (j.text.length * 0.01)), 1), FlxMath.lerp(j.scale.y, (4 - Math.abs(j.ID - curSelected)) * (0.3 - (j.text.length * 0.01)), 1));
-		}
-		if (new FlxRandom().bool(20 * keyCount)) FlxG.sound.play(Paths.sound("thud"));
-		curSelected = (curSelected > nameGroup.length - 1 ? 0 : (curSelected < 0 ? nameGroup.length - 1 : curSelected));
-		FlxG.sound.music.volume = 0.2 * (5-keyCount);
-		largePortrait.loadGraphic(Paths.image("credits/" + creditJSON[curSelected].name));
-		largePortrait.setGraphicSize(350, 350);
-		largePortrait.updateHitbox();
-		descText.text = (creditJSON[curSelected].name);
-		descText.text = (creditJSON[curSelected].description);
-		if (creditJSON[curSelected].social_link != null) {
-			var bitmap = BitmapData.loadFromFile('http://www.google.com/s2/favicons?domain=${creditJSON[curSelected].social_link}&sz=32');
-			bitmap.onComplete(function(bitmap) {
-				socialMediaFavicon.loadGraphic(bitmap);
-				socialMediaFavicon.visible = true;
-			});
-			socialMediaText.text = "Press enter to open social media link";
-		}
-		else {
-			socialMediaText.text = "Theres no social media link";
-			socialMediaFavicon.visible = false;
-		}	
+		});
+		FlxG.sound.play(Paths.sound("rumble"));	
+		antiSpam = true;
+		for (j in nameGroup)
+				j.scale.set(FlxMath.lerp(j.scale.x, (4 - Math.abs(j.ID - curSelected)) * (0.3 - (j.text.length * 0.01)), 1), FlxMath.lerp(j.scale.y, (4 - Math.abs(j.ID - curSelected)) * (0.3 - (j.text.length * 0.01)), 1));
 	}
+	if (new FlxRandom().bool(20 * keyCount)) FlxG.sound.play(Paths.sound("thud"));
+	curSelected = (curSelected > nameGroup.length - 1 ? 0 : (curSelected < 0 ? nameGroup.length - 1 : curSelected));
+	FlxG.sound.music.volume = 0.2 * (5-keyCount);
+	largePortrait.loadGraphic(Paths.image("credits/" + creditJSON[curSelected].name));
+	largePortrait.setGraphicSize(350, 350);
+	largePortrait.updateHitbox();
+	descText.text = (creditJSON[curSelected].name);
+	descText.text = (creditJSON[curSelected].description);
+	if (creditJSON[curSelected].social_link != null) {
+		var bitmap = BitmapData.loadFromFile('http://www.google.com/s2/favicons?domain=' + creditJSON[curSelected].social_link + 'sz=32');
+		bitmap.onComplete(function(bitmap) {
+			socialMediaFavicon.loadGraphic(bitmap);
+			socialMediaFavicon.visible = true;
+		});
+		socialMediaText.text = "Press enter to open social media link";
+	}
+	else {
+		socialMediaText.text = "Theres no social media link";
+		socialMediaFavicon.visible = false;
+	}	
+}
